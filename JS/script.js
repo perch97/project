d6a62eb403575b75e983b5cbf02356e93ce67cf0
Vadim <vadim.smir97@gmail.com>
2022-08-06 19:20:31 -0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18 yrs old?", "18");
+// console.log(answer+5);
+
+const answers = [];
+
+// answers[0] = prompt("Whats your name?",'');
+// answers[1] = prompt("Whats your last name?",'');
+// answers[2] = prompt("How old are you?",'');
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}`);
+
+// const name = "Vadim";
+
+// alert(`Hi${name}`);
+// console.log(2*4===+'8');
+
+const isChecked = true;
+      isClose = false;
+console.log(isChecked && isClose&&isClose);
